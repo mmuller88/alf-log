208602ee5d7ceca9f5d7bc2630d6f7823bc5e11f
Martin Müller <damadden88@googlemail.com>
2020-03-31 14:25:42 +0200

    init script

--- init.sh
+++ init.sh
@@ -12,5 +12,5 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
 node -e "console.log('Running Node.js ' + process.version)"
-npm install -g wait-on
+npm install
 
