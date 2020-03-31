62849f1b5e72539518a2126e18c2ea9839168dbc
Martin Müller <damadden88@googlemail.com>
2020-03-31 17:39:07 +0200

    init script

--- init.sh
+++ init.sh
@@ -9,7 +9,7 @@ curl -L https://github.com/docker/compose/releases/download/1.20.0/docker-compos
 chmod +x /usr/local/bin/docker-compose
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
+source /.nvm/nvm.sh
 nvm install node
 node -e "console.log('Running Node.js ' + process.version)"
 npm install
