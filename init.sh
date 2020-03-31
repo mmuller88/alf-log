59196ae5660cbb1d0804aa80aa618ca5dac1ac47
Martin Müller <damadden88@googlemail.com>
2020-03-31 09:43:48 +0200

    init script

---
+++ init.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+sudo yum update -y
+sudo amazon-linux-extras install docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.20.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+node -e "console.log('Running Node.js ' + process.version)"
+npm install -g wait-on
+
